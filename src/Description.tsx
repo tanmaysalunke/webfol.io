57265c30fab7efd3f5ac1382687318ee3a0df490
tanmaysalunke <tanmay442000@gmail.com>
2024-01-07 21:49:31 +0530

    Reduced dummy text

--- src/Description.tsx
+++ src/Description.tsx
@@ -15,17 +15,6 @@ const Description = () => {
         auctor. Duis id mollis justo, in volutpat nulla. Phasellus eget est id
         sapien consequat suscipit vitae eu dolor. Nulla in odio euismod lectus
         aliquam porttitor vitae tempus ipsum. Etiam interdum eu ante ut sodales.
-        <br />
-        Etiam at nisi in lectus facilisis semper sed eget lectus. Pellentesque
-        ac tempus nunc. Suspendisse mollis semper diam, vitae scelerisque justo
-        dapibus id. In hac habitasse platea dictumst. Nulla sed euismod magna.
-        Suspendisse potenti. Sed libero nisi, bibendum ac lacus ut, finibus
-        vehicula justo. Ut a pellentesque elit. In ex leo, eleifend vel laoreet
-        ac, pharetra ut ex. Vestibulum fringilla velit libero, non pulvinar ante
-        fringilla et. Fusce in imperdiet diam, vel ultricies nulla. Aliquam vel
-        vehicula mauris. Nullam felis lorem, suscipit at leo a, imperdiet
-        venenatis ante. Phasellus odio dolor, pretium ultricies congue ac,
-        convallis eu justo.
       </p>
     </section>
   );
